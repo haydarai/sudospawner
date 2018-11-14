d6d890b7d5b0bb36a8d9523be65d2f04877febd7
Haydar Ali Ismail <haydar_ai@yahoo.com>
2018-11-14 21:41:16 +0100

    Fix invalid json created

--- get_all_ports.sh
+++ get_all_ports.sh
@@ -1,9 +1,10 @@
-[ > ports.json
-for f in /home/*;
-    do {
-        user=${f##*/}
-        sudo -nu ${user} /usr/bin/find /home/${user}/.local/share/jupyter/runtime -name "*.json" -exec cat {} \; >> ports.json
-        , >> ports.json
-    }
-] >> ports.json
+echo "[" > ports.json
+for f in /home/*; do
+    user=${f##*/}
+    sudo -nu ${user} /usr/bin/find /home/${user}/.local/share/jupyter/runtime -name "*.json" | while read filename; do
+        sudo -nu ${user} cat ${filename} >> ports.json
+        echo "," >> ports.json
+    done
 done
+truncate -s-2 ports.json
+echo "]" >> ports.json
--- lrs.py
+++ lrs.py
@@ -1,6 +1,6 @@
 import json
 
-with open('../ports.json') as json_data:
+with open('./ports.json') as json_data:
     data = json.load(json_data)
     for entry in data:
         print(entry)
